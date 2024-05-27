a7902ba6a039ab37e53f782dec4f26c4bad08e46
EddieR222 <rivera.eduardo.2024@gmail.com>
2024-05-27 17:40:03 -0500

    Edited Player

--- Resources/Player Details/Player.gd
+++ Resources/Player Details/Player.gd
@@ -1,27 +1,48 @@
 class_name Player extends Resource
 
 """ Identifying Information """
-## The Name of the Player
-@export var Name: String
 
 ## The Unique ID of the Player
 @export var ID: int;
 
+## The Name of the Player
+@export var Name: String
 
 ## The NickName of the Player
 @export var NickName: String
 
 ## The BirthDate of the Player, stored as an array of integers in the (Month, Day, Year) format
-@export var BirthDate: Array[int]
+@export var BirthDate: Array[int];
 
-## The Age of the Player (in years)
-@export var Age: int
+## This contains a large amount of key details of the player.[br]
+## Every 8 bits contain different info about the player.[br]
+## Age:        Bits 0-7   (in years)[br]
+## Height:     Bits 8-15  (in cm)[br]
+## Weight:     Bits 16-23 (in kg)[br]
+## Rating:     Bits 24-31 (overall rating)[br]
+## Potential:  Bits 32-39 (potential)[br]
+## Foot:       Bits 40 (0= right, 1 = left)[br]
+## Skill Move: Bits 41-43 (1-5 stars)[br]
+## Weak Foot:  Bits 44-46 (1-5 stars)[br]
+## Morale:     Bits 47-54 (morale)[br]
+## Current Player Morale, how motivated or happy is the player. 
+## The BreakDown of the Morale is as follows
+## 1. Morale > 90  Very Happy
+## 2. Morale > 75  Happy
+## 3. Morale > 25  Neutral
+## 4. Morale < 25  UnHappy
+## 5. Morale < 10  Very UnHappy
+## Sharpness:  Bits 55-62 (sharpness)[br]
+## The current Sharpness of the Player.
+## The BreakDown of the Sharpness is as follows
+## 1. Sharpness > 90  Very Fit
+## 2. Sharpness > 75  Fit
+## 3. Sharpness > 25  Neutral
+## 4. Sharpness < 25  UnFit
+## 5. Sharpness < 10  Very UnFit
+@export
+var Key_Details: int = 0;
 
-## The Height of the Player (in cm)
-@export var Height: int
-
-## The Weight of the Player (in Kg)
-@export var Weight: int
 
 ## The Nationalities of the Player (array of territory ids)
 @export var Nationalities: Array[int]
@@ -37,12 +58,6 @@ class_name Player extends Resource
 
 
 """ Key Details """
-## The Overall Rating of the Player
-@export var Overall_Rating: int;
-
-## The Potential of the Player
-@export var Potential: int;
-
 ## The Market Value of the Player (in US Dollars, either in Millions or Thousands
 @export var Market_Value: float
 
@@ -52,36 +67,11 @@ class_name Player extends Resource
 ## The Weekly Wage the Player is Earning (always in thousands of US Dollars)
 @export var Wage: int
 
-## The Preferred Foot by the Player (0 = right, 1 = left, 2 = both)
-@export var Preferred_Foot: int
 
-## The Stars the Player has in Skill Moves
-@export var Skill_Moves: int
 
-## The Stars the PLayer has in Weak Foot (if 5 then player's preferred foot will become both)
-@export var Weak_Foot: int;
-
-## Current Player Morale, how motivated or happy is the player. 
-## The BreakDown of the Morale is as follows
-## 1. Morale > 90  Very Happy
-## 2. Morale > 75  Happy
-## 3. Morale > 25  Neutral
-## 4. Morale < 25  UnHappy
-## 5. Morale < 10  Very UnHappy
-@export var Morale: int
 
 ## The current training results; Higher the number the harder and better the player is training
 @export var Training: int
-
-## The current Sharpness of the Player.
-## The BreakDown of the Sharpness is as follows
-## 1. Sharpness > 90  Very Fit
-## 2. Sharpness > 75  Fit
-## 3. Sharpness > 25  Neutral
-## 4. Sharpness < 25  UnFit
-## 5. Sharpness < 10  Very UnFit
-@export var Sharpness: int
-
 
 ## This details if the player is healthy = 1, injured = 2, or injury prone = 3
 @export var Condition: int
@@ -109,3 +99,39 @@ class_name Player extends Resource
 ## Trophies Won
 @export var Trophies_Won: Dictionary;
 
+
+""" Player Setter Functions """
+## Call to set Player Name. String must be greater than 1 in length
+func set_player_name(name: String) -> void:
+	if name.length() <= 1:
+		return
+		
+	Name = name;
+
+## Sets the Player's ID
+func set_player_id(id: int) -> void:
+	ID = id;
+
+## Sets the Player's Age, must below 150
+func set_player_age(age: int) -> void:
+	# Return early if age is invalid
+	if age >= 150 or age < 0:
+		return
+		
+	# Now we set it but first we have to manipulate the bits
+	var age_bits: int = age & 0xFF;
+	
+	# Now we set the bits
+	Key_Details |= age_bits;
+
+## Sets the Player Height, must be below 255 or above 0
+func set_player_height(height: int) -> void:
+	# Return early if height is invalid
+	if height >= 255 or height < 0:
+		return
+		
+	# Now we set it but first we have to manipulate the bits
+	var height_bits: int = height & 0xFF;
+	
+	# Now we set the bits
+	Key_Details |= (height_bits << 8);
