a7686ec4bc5234f63d4e810518734b1abe7ce2e4
EddieR222 <rivera.eduardo.2024@gmail.com>
2024-08-09 04:53:01 -0500

    Add new icons and backgrounds. Also started on the new Team Editor

--- Scenes/GameMapEditor/New Team Editor/Team Editor 2.gd
+++ Scenes/GameMapEditor/New Team Editor/Team Editor 2.gd
@@ -3,16 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# First thing first we need to read in the settings
-	read_in_settings()
-	
-	
-	
-func read_in_settings() -> void:
-	# First we need to read in the colors
-	var background_color: ColorRect = get_node("BackgroundColor")
-	background_color.color = GameSettings.DEFAULT_SECONDARY_COLOR;
-
+	# First thing we want to do is load the countries
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
